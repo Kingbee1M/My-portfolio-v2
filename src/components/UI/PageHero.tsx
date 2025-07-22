2d638c62e0fe4bf87f47e9a9009415c326e5ec8c
Kingbee1M <160544114+Kingbee1M@users.noreply.github.com>
2025-07-23 00:06:33 +0100

    fixed a bug

--- src/components/UI/PageHero.tsx
+++ src/components/UI/PageHero.tsx
@@ -13,7 +13,7 @@ export default function PageHero() {
         <div className="absolute z-10 w-full h-[630px] backdrop-blur-sm bg-white/10 border border-white/30 rounded-lg p-6 shadow-lg"></div>
         <div className="absolute z-20 text-white text-3xl font-bold flex flex-col justify-center items-center gap-5">
             <p className="text-[40px] text-[var()] ">Welcome</p>
-            <p className="text-[25px] text-[var()]">I'm <span className="text-[var(--red-1)]">Idris</span> Habeeb</p>
+            <p className="text-[25px] text-[var()]">I&apos;m <span className="text-[var(--red-1)]">Idris</span> Habeeb</p>
             <p className="text-[15px] text-[var(--red-1)] py-2 px-4 rounded-[14px] bg-[var(--red-2)] ">Website & mibile App Frontend Developer</p>
     </div>
     </div>
